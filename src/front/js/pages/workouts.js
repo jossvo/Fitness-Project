f48e633a3e1eb4f1b40cd8c05dc5bab75083b7b2
Daniel Jimenez <daniel.jimenez.cr@gmail.com>
2023-02-18 00:08:40 +0000

    fix issues with filter by difficuly

--- src/front/js/pages/workouts.js
+++ src/front/js/pages/workouts.js
@@ -12,10 +12,13 @@ import {
   CDBSidebarFooter,
 } from "cdbreact";
 
-export const Workouts = () => {
+export const Workouts = () => {{}
   const { store, actions } = useContext(Context);
   const [searchName, setSearchName] = useState("");
   const [selectedDifficulty, setSelectedDifficulty] = useState("");
+  const [selectedWeeks, setSelectedWeeks] = useState("");
+
+
 
   useEffect(() => {
     async function fetchData() {
@@ -31,6 +34,11 @@ export const Workouts = () => {
   const inputDifficulty = (event) => {
     setSelectedDifficulty(event.target.value);
   };
+
+   const inputWeeks = (event) => {
+    setSelectedWeeks(event.target.value);
+  };
+
 
   return (
     <>
@@ -61,7 +69,6 @@ export const Workouts = () => {
                   Filter by Difficulty
                 </CDBSidebarMenuItem>
                 <CDBSidebarMenuItem className="mx-5 my-2">
-                  {" "}
                   <div>
                     <div className="form-check">
                       <input
@@ -116,7 +123,68 @@ export const Workouts = () => {
                     </div>
                   </div>
                 </CDBSidebarMenuItem>
+                <CDBSidebarMenuItem icon="calendar">
+                  Filter by Weeks
+                </CDBSidebarMenuItem>
+                 <CDBSidebarMenuItem className="mx-5 my-2">
+                  <div>
+                    <div className="form-check">
+                      <input
+                        className="form-check-input"
+                        type="radio"
+                        name="difficulty"
+                        id="easy"
+                        value="Easy"
+                        checked={selectedDifficulty === "Easy"}
+                        onChange={inputDifficulty}
+                      />
+                      <label
+                        className="form-check-label"
+                        htmlFor="exampleRadios1"
+                      >
+                        1-4
+                      </label>
+                    </div>
+                    <div className="form-check">
+                      <input
+                        className="form-check-input"
+                        type="radio"
+                        name="difficulty"
+                        id="medium"
+                        value="Medium"
+                        checked={selectedDifficulty === "Medium"}
+                        onChange={inputDifficulty}
+                      />
+                      <label
+                        className="form-check-label"
+                        htmlFor="exampleRadios2"
+                      >
+                        5-9
+                      </label>
+                    </div>
+                    <div className="form-check disabled">
+                      <input
+                        className="form-check-input"
+                        type="radio"
+                        name="difficulty"
+                        id="hard"
+                        value="Hard"
+                        checked={selectedDifficulty === "Hard"}
+                        onChange={inputDifficulty}
+                      />
+                      <label
+                        className="form-check-label"
+                        htmlFor="exampleRadios3"
+                      >
+                        +10
+                      </label>
+                    </div>
+                  </div>
+                </CDBSidebarMenuItem>
+
+
               </CDBSidebarMenu>
+
             </CDBSidebarContent>
 
             <CDBSidebarFooter
@@ -132,66 +200,66 @@ export const Workouts = () => {
 
           <div className="container  ">
             <div className="row ">
-              {store.workouts?.filter((element) =>element.name.toLowerCase().includes(searchName.toLowerCase()) ||
-                    element.coach_name.toLowerCase().includes(searchName.toLowerCase())
-                ).filter((element) => selectedDifficulty ? element.difficulty === selectedDifficulty : true).map((element, index) => (
-                  <div
-                    className="col-sm-12 col-md-6 col-lg-4 col-xl-3 my-3"
-                    key={element.id || index}
-                  >
-                    <div className="card">
-                      <h5 className="card-title-section text-center title-container">
-                        {element.name}
-                      </h5>
-                      <ul className="list-group list-group-flush list-group-workouts">
-                        <li>
-                          <img
-                            className="workout-library-img"
-                            src={element.wk_image}
-                            alt="Workout Image"
-                          />
-                        </li>
-                        <li className="list-group-item workout-item workout-description bg-light ">
-                          {element.description}
-                        </li>
-                        <li className="list-group-item workout-item">
-                          <i className="fa-solid fa-calendar-day mx-2"></i>
-                          <span>Days per week: </span>
-                          {element.days_per_week}
-                        </li>
-                        <li className="list-group-item workout-item bg-light ">
-                          <i className="fa-solid fa-calendar-days mx-2"></i>
-                          <span>Weeks: </span>
-                          {element.weeks}
-                        </li>
-                        <li className="list-group-item workout-item ">
-                          <i className="fa-solid fa-square-poll-vertical mx-2"></i>
-                          <span>Difficulty: </span>
-                          {element.difficulty}
-                        </li>
-                        <li className="list-group-item workout-item bg-light ">
-                          <i className="fa-solid fa-user mx-2"></i>
-                          <span>Coach: </span>
-                          {element.coach_name}
-                        </li>
-                      </ul>
-                    </div>
-                    <div className="btn-container-workouts my-3">
-                      <Link
-                        to="#"
-                        className="btn btn-warning btn-workout-library"
-                      >
-                        Buy
-                      </Link>
-                      <Link
-                        to={`/workout/${element.id}`}
-                        className="btn btn-secondary btn-workout-library"
-                      >
-                        Details
-                      </Link>
-                    </div>
+              {store.workouts?.filter((element) => element.name.toLowerCase().includes(searchName.toLowerCase()) ||
+                element.coach_name.toLowerCase().includes(searchName.toLowerCase())
+              ).filter((element) => selectedDifficulty ? element.difficulty === selectedDifficulty : true).map((element, index) => (
+                <div
+                  className="col-sm-12 col-md-6 col-lg-4 col-xl-3 my-3"
+                  key={element.id || index}
+                >
+                  <div className="card">
+                    <h5 className="card-title-section text-center title-container">
+                      {element.name}
+                    </h5>
+                    <ul className="list-group list-group-flush list-group-workouts">
+                      <li>
+                        <img
+                          className="workout-library-img"
+                          src={element.wk_image}
+                          alt="Workout Image"
+                        />
+                      </li>
+                      <li className="list-group-item workout-item workout-description bg-light ">
+                        {element.description}
+                      </li>
+                      <li className="list-group-item workout-item">
+                        <i className="fa-solid fa-calendar-day mx-2"></i>
+                        <span>Days per week: </span>
+                        {element.days_per_week}
+                      </li>
+                      <li className="list-group-item workout-item bg-light ">
+                        <i className="fa-solid fa-calendar-days mx-2"></i>
+                        <span>Weeks: </span>
+                        {element.weeks}
+                      </li>
+                      <li className="list-group-item workout-item ">
+                        <i className="fa-solid fa-square-poll-vertical mx-2"></i>
+                        <span>Difficulty: </span>
+                        {element.difficulty}
+                      </li>
+                      <li className="list-group-item workout-item bg-light ">
+                        <i className="fa-solid fa-user mx-2"></i>
+                        <span>Coach: </span>
+                        {element.coach_name}
+                      </li>
+                    </ul>
                   </div>
-                ))}
+                  <div className="btn-container-workouts my-3">
+                    <Link
+                      to="#"
+                      className="btn btn-warning btn-workout-library"
+                    >
+                      Buy
+                    </Link>
+                    <Link
+                      to={`/workout/${element.id}`}
+                      className="btn btn-secondary btn-workout-library"
+                    >
+                      Details
+                    </Link>
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
         </div>
